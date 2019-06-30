5e4239c3a8726c031124a467b662ef708ff2c904
makafernandez <mfernanlav@gmail.com>
2019-06-29 20:37:18 -0400

    test(DogsService): actualiza prueba unitaria

--- src/app/services/dogsService/dogs.service.spec.ts
+++ src/app/services/dogsService/dogs.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DogsService } from './dogs.service';
 
-fdescribe('DogsServiceService', () => {
+describe('DogsServiceService', () => {
   // We declare the variables that we'll use for the Test Controller and for our Service
   let httpTestingController: HttpTestingController;
   let service: DogsService;
@@ -31,7 +31,7 @@ fdescribe('DogsServiceService', () => {
   describe('#getBreedsList', () => {
     it('returned Observable should match the right data', () => {
       // Mock data
-      const mockBreedList = [{
+      const mockBreedList = {
         message: {
           affenpinscher: [],
           african: [],
@@ -55,7 +55,7 @@ fdescribe('DogsServiceService', () => {
           ]
         },
         status: 'success'
-      }];
+      };
 
       // Subscribe to service and define expected response
       service.getBreedsList()
@@ -69,6 +69,64 @@ fdescribe('DogsServiceService', () => {
       expect(req.request.method).toBe('GET');
 
       req.flush(mockBreedList);
+    });
+  });
+
+  describe('#getSubBreedsList', () => {
+    it('returned Observable should match the right data', () => {
+      // Mock data
+      const mockBreedList = {
+        message: [
+          'afghan',
+          'basset',
+          'blood',
+          'english',
+          'ibizan',
+          'walker'
+        ],
+        status: 'success'
+      };
+
+      // Subscribe to service and define expected response
+      service.getSubBreedsList('bulldog')
+        .subscribe(response => {
+          expect(response).toEqual(mockBreedList);
+        });
+
+      const req = httpTestingController.expectOne(
+        'https://dog.ceo/api/breed/bulldog/list'
+      );
+      expect(req.request.method).toBe('GET');
+
+      req.flush(mockBreedList);
+    });
+  });
+
+  describe('#getBreedImg', () => {
+    it('returned Observable should match the right data', () => {
+      // Mock data
+      const mockBreedImgList = {
+        message: [
+          'https://images.dog.ceo/breeds/hound-afghan/n02088094_1259.jpg',
+          'https://images.dog.ceo/breeds/hound-afghan/n02088094_5927.jpg',
+          'https://images.dog.ceo/breeds/hound-basset/n02088238_5239.jpg'
+        ],
+        status: 'success'
+      };
+
+      // Subscribe to service and define expected response
+      service.getBreedImg('hound')
+        .subscribe(response => {
+          expect(response.name).toEqual('hound');
+          expect(response.url[0]).toEqual('https://images.dog.ceo/breeds/hound-afghan/n02088094_1259.jpg');
+        });
+
+      const req = httpTestingController.expectOne(
+        'https://dog.ceo/api/breed/hound/images/random/10'
+      );
+      expect(req.request.method).toBe('GET');
+
+      req.flush(mockBreedImgList);
     });
   });
 });
