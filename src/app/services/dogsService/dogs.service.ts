ce347ddbbd4f29f53f1e75624472132891dc9bc8
makafernandez <mfernanlav@gmail.com>
2019-06-29 20:37:44 -0400

    Agrega cometarios para documentación

--- src/app/services/dogsService/dogs.service.ts
+++ src/app/services/dogsService/dogs.service.ts
@@ -4,10 +4,9 @@ import { Observable } from 'rxjs';
 import { take, map } from 'rxjs/operators';
 
 /**
- * @description Sends requests to the Dog API and returns the responses to the front.
- * @author makafernandez
- * @date 2019-06-27
- * @export
+ * Sends requests to the Dog API and returns the responses
+ * to the client.
+ *
  */
 @Injectable({
   providedIn: 'root'
@@ -16,17 +15,41 @@ export class DogsService {
   url = 'https://dog.ceo/api';
   allBreeds = '/breeds/list/all';
   allSubBreed = '/breed/hound/list';
-
+  /**
+   * Creates an instance of DogsService.
+   * @param {HttpClient} http
+   * @memberof DogsService
+   */
   constructor(private http: HttpClient) { }
-
+  /**
+   * Gets a list with all the breeds and sub-breeds
+   *
+   * @returns {Observable<any>}
+   * @memberof DogsService
+   */
   getBreedsList(): Observable<any> {
     return this.http.get(`${this.url}${this.allBreeds}`).pipe(take(1));
   }
-
+  /**
+   * Gets a list of all the sub-breeds, from a particular breed.
+   *
+   * @param {string} breed
+   * @returns {Observable<any>}
+   * @memberof DogsService
+   */
   getSubBreedsList(breed: string): Observable<any> {
     return this.http.get(`${this.url}/breed/${breed}/list`).pipe(take(1));
   }
-
+  /**
+   * Gets a list of random image urls from a particular breed.
+   * By default, it will return a list of 10 elements, unless
+   * a different number is passed as a parameter.
+   *
+   * @param {string} breed
+   * @param {number} [num=10]
+   * @returns {Observable<any>}
+   * @memberof DogsService
+   */
   getBreedImg(breed: string, num = 10): Observable<any> {
     return this.http
       .get(`${this.url}/breed/${breed}/images/random/${num}`)
@@ -41,7 +64,17 @@ export class DogsService {
         })
       );
   }
-
+  /**
+   * Get a list of random image urls from a particular sub-breed,
+   * By default, it will return a list of 10 elements, unless
+   * a different number is passed as a parameter.
+   *
+   * @param {string} breed
+   * @param {string} subBreed
+   * @param {number} [num=10]
+   * @returns {Observable<any>}
+   * @memberof DogsService
+   */
   getSubBreedImg(breed: string, subBreed: string, num = 10): Observable<any> {
     return this.http
       .get(`${this.url}/breed/${breed}/${subBreed}/images/random/${num}`)
