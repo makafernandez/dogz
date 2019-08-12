822537a83716219a5359e1453b5c32e424b172e5
makafernandez <mfernanlav@gmail.com>
2019-08-12 05:49:55 -0300

    Implementa capa Core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-
-
+import { CoreModule } from './core/core.module';
+import { DataModule } from './data/data.module';
+import { DogBreedsRepository } from './core/repositories/dog-breeds.repository';
+import { DogBreedsWebRepository } from './data/repository/dog-breeds-repository/dog-breeds-web-repository';
+import { DogsService } from './services/dogsService/dogs.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 import { PresentationModule } from './presentation/presentation.module';
 import { SharedService } from './services/sharedService/shared.service';
-import { DogsService } from './services/dogsService/dogs.service';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    PresentationModule,
-    AppRoutingModule,
-    MatToolbarModule
-  ],
-  providers: [SharedService, DogsService],
-  bootstrap: [AppComponent],
-  exports: []
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		PresentationModule,
+		AppRoutingModule,
+		MatToolbarModule,
+		DataModule,
+		CoreModule
+	],
+	providers: [SharedService, DogsService, { provide: DogBreedsRepository, useClass: DogBreedsWebRepository }],
+	bootstrap: [AppComponent],
+	exports: []
 })
-export class AppModule {}
+export class AppModule { }
